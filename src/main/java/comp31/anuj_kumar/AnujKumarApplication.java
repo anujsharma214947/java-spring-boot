a15a8d89c5cb45b437dd9af8c3de9f0571a7ad50
Anuj Kumar <anujsharma01214947@gmail.com>
2023-10-21 04:49:57 -0400

    week one lab

---
+++ src/main/java/comp31/anuj_kumar/AnujKumarApplication.java
@@ -0,0 +1,13 @@
+package comp31.anuj_kumar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnujKumarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnujKumarApplication.class, args);
+	}
+
+}
